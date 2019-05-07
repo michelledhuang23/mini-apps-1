800e4c8cc79bc0a6edf40f99679929f3ebe3820b
Michelle <michelledhuang@ucla.edu>
2019-05-07 16:24:23 -0700

    Create html layout, post request, and parse data into csv format

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,20 @@
 
+$("#jsonForm").submit(function(e) {
 
+  e.preventDefault(); // avoid to execute the actual submit of the form.
+
+  var url = 'http://localhost:3000/';
+
+  $.ajax({
+         type: "POST",
+         url: url,
+         data: {json: $("#jsonData").val()
+          },
+         success: function(data)
+         {
+           console.log('test');
+         }
+       });
+
+
+});
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,5 +1,48 @@
-const express = require("express");
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = 3000;
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('./client'));
 
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/', (req, res) => {
+  res.status(200).send();
+});
+
+app.post('/', (req, res) => {
+  res.status(201).send();
+  let csvData = '';
+  let tempRow = [];
+  let headers = [];
+  let parsedData = JSON.parse(req.body.json);
+  for (const key in parsedData) {
+    if (key !== 'children') {
+      headers.push(key);
+      tempRow.push(parsedData[key]);
+    }
+  }
+  csvData += headers.join(',') + '\n';
+  csvData += tempRow.join(',') + '\n';
+  tempRow = [];
+  let helper = function(arr) {
+    if (arr.length > 0) {
+      for (let i = 0; i < arr.length; i++) {
+        for (let value of headers) {
+          if (arr[i].hasOwnProperty(value)) {
+            tempRow.push(arr[i][value]);
+          }
+        }
+        csvData += tempRow.join(',') + '\n';
+        tempRow = [];
+        helper(arr[i].children);
+      }
+    }
+  };
+  helper(parsedData.children);
+  console.log(csvData);
+});
