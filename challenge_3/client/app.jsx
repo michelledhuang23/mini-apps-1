f581c5d6f4a7598ab094720416db243a89e36da1
Michelle <michelledhuang@ucla.edu>
2019-05-08 17:30:57 -0700

    Set up components rendering and submit functions

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -3,29 +3,74 @@ import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   
-  constructor() {
-    return super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: 'home'
+    }
+    this.getUsersPage = this.getUsersPage.bind(this);
+    this.submitUsersInfo = this.submitUsersInfo.bind(this);
+    this.submitAddressInfo = this.submitAddressInfo.bind(this);
+    this.submitPaymentInfo = this.submitPaymentInfo.bind(this);
   }
-  
+
+  getCurrentPage() {
+    switch (this.state.page) {
+      case 'users':
+        return <Form1 submitUsersInfo={this.submitUsersInfo}/>
+      case 'address':
+        return <Form2 submitAddressInfo={this.submitAddressInfo}/>
+      case 'payment':
+        return <Form3 submitPaymentInfo={this.submitPaymentInfo}/>
+    }
+  }
+
+  getUsersPage() {
+    this.setState({
+      page: 'users'
+    });
+  }
+
+  submitUsersInfo() {
+    this.setState({
+      page: 'address'
+    }, () => {
+      
+    });
+
+  }
+
+  submitAddressInfo() {
+    this.setState({
+      page: 'payment'
+    });
+  }
+
+  submitPaymentInfo() {
+    this.setState({
+      page: 'home'
+    });
+  }
+
   render() {
+    const currentlyDisplayedForm = this.getCurrentPage();
+    let button = this.state.page === 'home' ? <button onClick={this.getUsersPage}>Checkout</button> : null;
     return (<div>
-      <h2>Multistep Checkout Experience</h2>
-      <button>Checkout</button>
-      <div className='usersInfo'><Form1 /></div>
-      <div className='addressInfo'><Form2 /></div>
-      <div className='paymentInfo'><Form3 /></div>
+        <h2>Multistep Checkout Experience</h2>
+        {button}
+      {currentlyDisplayedForm}
     </div>);
   }
 }
 
 class Form1 extends React.Component {
 
-  constructor() {
-    return super();
+  constructor(props) {
+    super(props);
   }
   
   render() {
-    return (<div>
+    return (<div className='users-info'>
       <h2>Create Account</h2>
       <div>
         <label>
@@ -47,19 +92,19 @@ class Form1 extends React.Component {
           <input type="text" className='password'/>
         </label>
       </div>
-      <button className='usersButton'>Submit</button>
+      <button className='users-button' onClick={this.props.submitUsersInfo}>Submit</button>
     </div>);
   }
 }
 
 class Form2 extends React.Component {
 
-  constructor() {
-    return super();
+  constructor(props) {
+    super(props);
   }
   
   render() {
-    return (<div>
+    return (<div className='address-info'>
       <h2>Shipping Information</h2>
       <div>
         <label>
@@ -78,55 +123,55 @@ class Form2 extends React.Component {
       <div>
         <label>
           City:
-          <input type="text" className='password'/>
+          <input type="text" className='city'/>
         </label>
       </div>
 
       <div>
         <label>
           State:
-          <input type="text" className='password'/>
+          <input type="text" className='state'/>
         </label>
       </div>
 
       <div>
         <label>
           Zip Code:
-          <input type="text" className='password'/>
+          <input type="text" className='zip-code'/>
         </label>
       </div>
 
       <div>
         <label>
           Phone Number:
-          <input type="text" className='password'/>
+          <input type="text" className='phone-number'/>
         </label>
       </div>
-      <button className='addressButton'>Submit</button>
+      <button className='address-button' onClick={this.props.submitAddressInfo}>Submit</button>
     </div>);
   }
 }
 
 class Form3 extends React.Component {
 
-  constructor() {
-    return super();
+  constructor(props) {
+    super(props);
   }
   
   render() {
-    return (<div>
+    return (<div className='payment-info'>
       <h2>Payment Information</h2>
       <div>
         <label>
           Credit Card #:
-          <input type="text" className='creditCard'/>
+          <input type="text" className='credit-card'/>
         </label>
       </div>
 
       <div>
         <label>
           Expiration Date:
-          <input type="text" className='expirationDate'/>
+          <input type="text" className='expiration-date'/>
         </label>
       </div>
 
@@ -140,10 +185,10 @@ class Form3 extends React.Component {
       <div>
         <label>
           Billing Zip Code:
-          <input type="text" className='billingZipCode'/>
+          <input type="text" className='billing-zip-code'/>
         </label>
       </div>
-      <button className='paymentButton'>Submit</button>
+      <button className='payment-button' onClick={this.props.submitPaymentInfo}>Submit</button>
     </div>);
   }
 }
